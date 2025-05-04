02a3408efe2564ba4c98ef426b1136b2939acc21
IgorMedvedenko <noveciti0@gmail.com>
2025-05-04 17:46:10 +0300

    fix1

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,13 +1,21 @@
-import readContacts from '../utils/readContacts.js';
-import writeContacts from '../utils/writeContacts.js';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PATH_DB = path.join(__dirname, '../db/db.json');
 
 const removeLastContact = async () => {
   try {
-    const contacts = await readContacts();
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
     if (contacts.length > 0) {
       contacts.pop();
-      await writeContacts(contacts);
-      console.log('Successfully removed the last contact from db.json');
+      await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), `utf8`);
+      console.log('Last contact removed successfully.');
     } else {
       console.log('No contacts to remove.');
     }
